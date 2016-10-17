ed99f83c11e448c437a3b6d959b5c26017faed0c
unknown <fabian.conte@ug.uchile.cl>
2016-10-16 23:50:35 -0300

    Arreglo filtro

--- PasaBanda.m
+++ PasaBanda.m
@@ -1,14 +1,11 @@
 function datos_filtrados=PasaBanda(data,B1,B2)
-fe=8000/2; %frecuencia de Shannon//frec de nyquist
+fe=10000/2; %frecuencia de Shannon//frec de nyquist
 
-Wp=[B1 B2]/fe; %Transformar las bandas de frecuencia
-Ws=[B1-200 B
-    2+200]/fe;
- % Atenuación en la banda de paso
- % Atenuación en la banda de rechazo
-[n,Wn]=buttord(Wp,Ws,3,40);%Calcular el orden del filtro
+Wp=B1/fe; %Transformar las bandas de frecuencia
+Ws=B2/fe;
 %coeficientes del filtro
-[b,a]=butter(n,Wn); %n=orden del filtro, Wn=frecuencia de corte
+[b,a]=butter(7,[Wp Ws],'bandpass'); %n=orden del filtro, Wn=frecuencia de corte
+freqz(b,a);
 datos_filtrados=filter(b,a,data);
 end
 
