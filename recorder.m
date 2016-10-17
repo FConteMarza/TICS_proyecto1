01de1028a9c9e35f2fce21f608bad1b756c2a625
ftomas <tomas.ferrer-@hotmail.com>
2016-10-17 19:25:58 -0300

    recorder

--- recorder.m
+++ recorder.m
@@ -4,7 +4,7 @@ variables;
 % Record your voice for 5 seconds.
 recObj = audiorecorder;
 disp('Start speaking.')
-recordblocking(recObj, 10);%T tiempo a grabar
+recordblocking(recObj, 20);%T tiempo a grabar
 disp('End of Recording.');
 
 % Play back the recording.
@@ -19,10 +19,16 @@ plot(myRecording);
 canal1=PasaBanda(myRecording, F1-50,F1+210);%fl y fu las frecuenciias en las que se encuentra el canal
 canal2=PasaBanda(myRecording, F2-50,F2+210);
 canal3=PasaBanda(myRecording, F3-50,F3+210);
+
+%CANAL 1
 %Reconocer Header
 noheaduno=Hederd(canal1,F1,Df,Fs,Dt);
-data=Taild(noheaduno,F1,Df,Fs,Dt);
-data_bits=DFSK(data,F1,Df,Fs,Dt);
+%Reconocer cola
+data=Taild(noheaduno,F1,Df,Fs,Dt);%datos foto 
+
+data_bits=DFSK(data,F1,Df,Fs,Dt);%datos en bits
+matriz_im=recoveryimage(data_bits);%matriz para JPG
+imshow(matriz_im)
 
 
 
