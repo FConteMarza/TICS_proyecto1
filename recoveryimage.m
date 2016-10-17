31e9cf2cc1e76685d17a545530b0cee9877f25ab
FConteMarza <fabian.conte@ug.uchile.cl>
2016-10-17 19:06:05 -0300

    recoveryimage

---
+++ recoveryimage.m
@@ -0,0 +1,24 @@
+function [R]=recoveryimage(Data)
+%data forma 'estrella.jpg'
+
+
+MN =[21 24]; % tamaño imagen
+
+l1 = MN(:,2);
+
+l2= MN(:,1);  %21
+
+image=zeros(21,24);
+
+n=1;
+
+for i=1:l1  % de i a 24 columna
+
+for j=1:l2      
+        image(j,i)=Data(n);  
+        n=n+1;        
+    end
+    
+end
+
+
